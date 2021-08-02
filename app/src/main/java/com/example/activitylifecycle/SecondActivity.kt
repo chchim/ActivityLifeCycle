5e2ec443a7ae347c30078da03b31053d2de8a570
cch25 <Chim0205123_>
2021-08-02 14:12:52 +0800

    Initial commit

---
+++ app/src/main/java/com/example/activitylifecycle/SecondActivity.kt
@@ -0,0 +1,56 @@
+package com.example.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    var count : Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val tv : TextView = findViewById(R.id.tvResult)
+        Log.i("myActivity", "OnCreate")
+
+        if(savedInstanceState !=null){
+
+           count= savedInstanceState.getInt("count")
+            tv.text = count.toString()
+        }
+
+        val btn :Button = findViewById(R.id.btnAdd)
+
+        btn.setOnClickListener(){
+            count +=1
+            tv.text = count.toString()
+        }
+
+    }
+
+    override fun onStart(){
+        super.onStart()
+        Log.i("myActivity", "OnStart")
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.i("myActivity", "OnResume")
+    }
+
+    override fun onStop(){
+        super.onStop()
+        Log.i("myActivity", "onStop")
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.i("myActivity", "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("count",count)
+    }
+}
